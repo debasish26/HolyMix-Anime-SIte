89f576b94d4b489787465108fe2f57230eb6b07f
Prasenjit Mohanta <106687836+prasenjitmohanta@users.noreply.github.com>
2024-03-25 13:35:09 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -7,7 +7,7 @@ const recommendationsapi = "/recommendations/";
 // Api Server Manager
 
 const AvailableServers = [
-    "https://api.mahantad024.workers.dev";
+    "https://api.mahantad024.workers.dev",
 ];
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
